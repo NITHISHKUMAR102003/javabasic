e72af7259f13a8f5d6bd0187acd12ac24ded0d61
User <User@DESKTOP-VVBKD2T>
2024-06-27 11:54:13 +0530

    2nd project

--- nk/src/nk1/sample.java
+++ nk/src/nk1/sample.java
@@ -3,8 +3,16 @@ package nk1;
 public class sample {
 
 	public static void main(String[] args) {
-		System.out.print("welcome");
-		// TODO Auto-generated method stub
+		char ch='a';
+		System.out.println(ch);
+		char ch1=65;
+		System.out.println(ch1);
+		char ch2='\u00A7';
+		System.out.println(ch2);
+		char ch3='\u20AC';
+		System.out.println(ch3);
+		int a='A';
+		System.out.println(a);
 
 	}
 
